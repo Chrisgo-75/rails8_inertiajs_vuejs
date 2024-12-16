5215f3c2b203375a435c2b1e652e42fb55af778d
Chris Arndt <christopher.arndt@wisc.edu>
2024-12-16 14:43:36 -0600

    Initial Commit.

---
+++ scripts/install_nodejs.sh
@@ -0,0 +1,7 @@
+# It doesn't seem that I need to run the below commented out commands.
+# apt-get remove nodejs -y
+# apt-get purge nodejs -y
+# apt-get autoremove -y
+# apt-get update
+curl -sL https://deb.nodesource.com/setup_22.x | bash
+apt-get install nodejs -y
